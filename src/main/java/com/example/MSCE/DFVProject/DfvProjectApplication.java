cf5d23050fb555e2ff9b3198236eb81878eb4ebe
Sebastien Gaillard <sebastien.gaillard@acedigitale.com>
2023-12-19 09:57:59 +0100

    Startup

---
+++ src/main/java/com/example/MSCE/DFVProject/DfvProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.MSCE.DFVProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfvProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfvProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MSCE/DFVProject/DfvProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSCE.DFVProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfvProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
